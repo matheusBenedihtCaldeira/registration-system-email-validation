a79595173bb9d1110008ab92e756024bd8a0f127
bene <matheusbenediht@gmail.com>
2023-11-28 16:21:36 -0300

    First commit

---
+++ src/test/java/com/registration/system/RegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registration.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
